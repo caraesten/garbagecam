ab1bc2f5566ab617bc210cc546351efdf113bff5
Esten Hurtle <dondeesten@gmail.com>
2016-08-21 12:19:30 +0300

    Initial Commit

---
+++ StripCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StripCam
+//
+//  Created by Esten Hurtle on 8/21/16.
+//  Copyright © 2016 Esten Hurtle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
